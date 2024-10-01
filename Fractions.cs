0f0b47e04396d7a0077ca277b7f8c80700aae89c
Jazer <saltanhayrliev2006@gmail.com>
2024-10-01 23:59:21 +0300

    main func Fractions

---
+++ Fractions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction_1_exercise_OOP
+{
+    public class Class1
+    {
+    }
+}
